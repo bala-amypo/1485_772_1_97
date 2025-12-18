4a9f69b1b70268080519edf01774b84f6087b7e0
bala-amypo <bala@amypo.in>
2025-12-18 17:56:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -10,54 +10,32 @@ public class CertificateTemplate {
     private Long id;
 
     private String templateName;
-    private String backgroundUrl;
-    private String fontStyle;
-    private String signatureName;
+    private String description;
 
     public CertificateTemplate() {
-    }
-
-    public CertificateTemplate(String templateName, String backgroundUrl,
-                               String fontStyle, String signatureName) {
-        this.templateName = templateName;
-        this.backgroundUrl = backgroundUrl;
-        this.fontStyle = fontStyle;
-        this.signatureName = signatureName;
     }
 
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTemplateName() {
         return templateName;
     }
-  
+
     public void setTemplateName(String templateName) {
         this.templateName = templateName;
     }
-  
-    public String getBackgroundUrl() {
-        return backgroundUrl;
+
+    public String getDescription() {
+        return description;
     }
-  
-    public void setBackgroundUrl(String backgroundUrl) {
-        this.backgroundUrl = backgroundUrl;
-    }
-  
-    public String getFontStyle() {
-        return fontStyle;
-    }
-  
-    public void setFontStyle(String fontStyle) {
-        this.fontStyle = fontStyle;
-    }
-  
-    public String getSignatureName() {
-        return signatureName;
-    }
-  
-    public void setSignatureName(String signatureName) {
-        this.signatureName = signatureName;
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
+++ src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
@@ -1,4 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.CertificateTemplate;
+
 public interface CertificateTemplateRepository
         extends JpaRepository<CertificateTemplate, Long> {
-    CertificateTemplate findByTemplateName(String templateName);
 }
