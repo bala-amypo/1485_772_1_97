fc94985e4bce559aaed36d8a7ed226307d1038a8
bala-amypo <bala@amypo.in>
2025-12-18 18:17:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,52 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String rollNumber;
+
+    public Student() {
+    }
+
+    public Student(String name, String email, String rollNumber) {
+        this.name = name;
+        this.email = email;
+        this.rollNumber = rollNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+  
+    public void setName(String name) {
+        this.name = name;
+    }
+  
+    public String getEmail() {
+        return email;
+    }
+  
+    public void setEmail(String email) {
+        this.email = email;
+    }
+  
+    public String getRollNumber() {
+        return rollNumber;
+    }
+  
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+}
