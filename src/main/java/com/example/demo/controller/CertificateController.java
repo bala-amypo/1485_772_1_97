2c4498fdaf5f277adff71cb158b61671f105aae6
bala-amypo <bala@amypo.in>
2025-12-18 17:44:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/CertificateController.java
+++ src/main/java/com/example/demo/controller/CertificateController.java
@@ -1,30 +1,22 @@
 package com.example.demo.controller;
 
-import java.util.List;
-import com.example.demo.entity.Certificate;
-import com.example.demo.service.CertificateService;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.entity.Certificate;
+import com.example.demo.service.CertificateService;
+
 @RestController
+@RequestMapping("/certificate")
 public class CertificateController {
 
     @Autowired
     private CertificateService certificateService;
 
-    @PostMapping("/certificates")
-    public Certificate generateCertificate(@RequestBody Certificate certificate) {
-        return certificateService.generateCertificate(certificate);
-    }
-
-    @GetMapping("/certificates")
-    public List<Certificate> getCertificates() {
-        return certificateService.getAllCertificates();
-    }
-
-    @GetMapping("/verify/{verificationCode}")
-    public Certificate verifyCertificate(@PathVariable String verificationCode) {
-        return certificateService.verifyCertificate(verificationCode);
+    @GetMapping("/verify/{code}")
+    public Optional<Certificate> verifyCertificate(@PathVariable String code) {
+        return certificateService.verifyCertificate(code);
     }
 }
