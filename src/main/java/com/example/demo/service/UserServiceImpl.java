86a6cbbfa5c854bcfd86b76e826aea9336efecc2
bala-amypo <bala@amypo.in>
2025-12-26 09:31:59 +0000

    User commit

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -4,9 +4,9 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-@service
-import java.util.Optional;
 
+import java.util.Optional;
+@service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
